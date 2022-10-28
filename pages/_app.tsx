e70c33e0483a75b10154723071f60e305646f2cb
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2022-10-28 20:04:15 +0000

    Update Footer.tsx and _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,19 @@ import { SessionProvider } from "next-auth/react";
 import Header from "../components/Header";
 import { RecoilRoot } from "recoil";
 import Footer from "../components/Footer";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+  const router = useRouter();
+  const { pathname } = router;
+  const show = router.pathname === "/";
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
         <Header />
         <Component {...pageProps} />
       </RecoilRoot>
-      <Footer />
+      {show && <Footer />}
     </SessionProvider>
   );
 }
